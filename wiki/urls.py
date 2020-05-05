bbb8190c48279e0dfbcb0cd6c98ecd5772c15c6e
nickKearns <nickkearns133@gmail.com>
2020-05-05 15:56:22 -0400

    auth added

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from wiki.views import PageListView, PageDetailView
 
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
+    path('accounts/', include('django.contrib.auth.urls')),    
+
 ]
